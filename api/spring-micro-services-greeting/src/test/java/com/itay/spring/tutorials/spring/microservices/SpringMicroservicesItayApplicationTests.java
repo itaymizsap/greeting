1151ee091f7a73d5cded7829bdbe1e2451946ee5
itaymizsap@Optics1 <itaymizsap@gmail.com>
2018-02-18 19:00:50 +0200

    adding comment

--- api/spring-micro-services-greeting/src/test/java/com/itay/spring/tutorials/spring/microservices/SpringMicroservicesItayApplicationTests.java
+++ api/spring-micro-services-greeting/src/test/java/com/itay/spring/tutorials/spring/microservices/SpringMicroservicesItayApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringMicroservicesItayApplicationTests {
-
+//TEst class
 	@Test
 	public void contextLoads() {
 	}
