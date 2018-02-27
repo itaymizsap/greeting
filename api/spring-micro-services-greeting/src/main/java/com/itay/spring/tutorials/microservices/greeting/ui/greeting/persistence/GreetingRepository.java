758e58c6cf7e9416b94a475f759f3be8c43f9801
itaymizsap@Optics1 <itaymizsap@gmail.com>
2018-02-27 13:42:56 +0200

    rearrange

--- api/spring-micro-services-greeting/src/main/java/com/itay/spring/tutorials/microservices/greeting/ui/greeting/persistence/GreetingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itay.spring.tutorials.microservices.greeting.ui.greeting.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepository extends CrudRepository<Greeting, Integer> {}
